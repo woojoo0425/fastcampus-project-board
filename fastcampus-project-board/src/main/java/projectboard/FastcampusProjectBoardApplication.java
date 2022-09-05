af45ad9ee8d16f8f88f6538e27f96667ac2d6aba
woojoo <shinwoojoo1995@daum.net>
2022-09-05 11:31:57 +0900

    스프링부트 프로젝트 시작
    스프링부트 프로젝트 초기화

---
+++ fastcampus-project-board/src/main/java/projectboard/FastcampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectBoardApplication.class, args);
+    }
+
+}
