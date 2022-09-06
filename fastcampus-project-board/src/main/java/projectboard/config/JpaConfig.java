1cab76ba28db53dd1c3f8ead8c843fd66079ca76
woojoo <shinwoojoo1995@daum.net>
2022-09-06 21:53:12 +0900

    mysql 설치와 유저&권한 설정

---
+++ fastcampus-project-board/src/main/java/projectboard/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("wj");
+    }
+}
