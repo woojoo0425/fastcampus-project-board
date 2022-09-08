655826c683488ab0605e839792c7ef34cf7ab753
woojoo <shinwoojoo1995@daum.net>
2022-09-08 13:50:19 +0900

    header, footer 수정

--- fastcampus-project-board/src/test/java/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/projectboard/controller/ArticleControllerTest.java
@@ -35,7 +35,7 @@ class ArticleControllerTest {
     public void given_whenArticleDetailView_them() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(view().name("articleComments"))
                 .andExpect(model().attributeExists("article"));
@@ -48,7 +48,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -58,6 +58,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
