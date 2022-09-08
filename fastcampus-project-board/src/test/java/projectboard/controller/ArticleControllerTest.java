766c06993faf4fc9588adc4639491c13ab6f9dd4
woojoo <shinwoojoo1995@daum.net>
2022-09-08 11:58:40 +0900

    TEST 컨트롤러 업데이트

--- fastcampus-project-board/src/test/java/projectboard/controller/ArticleControllerTest.java
+++ fastcampus-project-board/src/test/java/projectboard/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)
@@ -20,28 +19,46 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @DisplayName("View GET 게시글 상세 페이지 - 정상 호출")
+    @Disabled("구현 중")
+    @DisplayName("View GET 게시글 리스트 페이지 - 정상 호출")
     @Test
-    public void given_whenArticleView_them() throws Exception {
-        mvc.perform(get("/articles/1"))
+    public void given_whenArticleListView_them() throws Exception {
+        mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("article"));
     }
 
+    @Disabled("구현 중")
+    @DisplayName("View GET 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void given_whenArticleDetailView_them() throws Exception {
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(view().name("articleComments"))
+                .andExpect(model().attributeExists("article"));
+    }
+
+    @Disabled("구현 중")
     @DisplayName("View GET 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     public void given_whenArticleSearchView_them() throws Exception {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("articles/search"))
                 .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 
+    @Disabled("구현 중")
     @DisplayName("View GET 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     public void given_whenArticleHashtagSearchView_them() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
+                .andExpect(view().name("articles/search-hashtag"))
                 .andExpect(content().contentType(MediaType.TEXT_HTML));
     }
 }
